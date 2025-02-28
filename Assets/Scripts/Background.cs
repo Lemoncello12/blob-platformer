e4f652e59ac4dac6bd895b188df971f4ebf17ac5
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2025-02-27 20:23:59 -0800

    Improved background generator

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -10,9 +10,12 @@ public class Background : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        for (int i = 1; i <= numRight; i++)
+        for (int i = 0; i < numRight; i++)
         {
-            Instantiate(background, new Vector3(i * 48 + transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+            for (int a = 0; a < numUp; a++)
+            {
+                Instantiate(background, new Vector3(i * 48 + transform.position.x, a * 27 + transform.position.y, transform.position.z), Quaternion.identity);
+            } 
         }
     }
 }
