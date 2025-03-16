0f19444916f012131cc54ea1b7f887a4497c1173
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2025-03-16 10:15:47 -0700

    Started adding menu ui

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+
+    }
+
+    public void LoadScene(int scene)
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
