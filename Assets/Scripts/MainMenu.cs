efc70dd843d3b125b05a08915ec6c1bc954d4a5d
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2025-03-20 20:18:04 -0700

    Made functioning level select

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject main;
+    public GameObject levelSelect;
+    public GameObject options;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        main.SetActive(true);
+        levelSelect.SetActive(false);
+        options.SetActive(false);
     }
 
     // Update is called once per frame
@@ -16,19 +22,28 @@ public class MainMenu : MonoBehaviour
         
     }
 
+    public void Back()
+    {
+        levelSelect.SetActive(false);
+        options.SetActive(false);
+        main.SetActive(true);
+    }
+
     public void Play()
     {
-
+        main.SetActive(false);
+        levelSelect.SetActive(true);
     }
 
     public void LoadScene(int scene)
     {
-
+        SceneManager.LoadScene(scene);
     }
 
     public void Options()
     {
-
+        main.SetActive(false);
+        options.SetActive(true);
     }
 
     public void Quit()
